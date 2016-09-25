25105a4c468029f18e93375ffd0ac2d6a24a0402
Chris Hedberg <christophhedberg@gmail.com>
2016-09-25 16:51:09 -0500

    Loading a second scene from a button click.

---
+++ Assets/Scripts/LoadSceneFromButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadSceneFromButton : MonoBehaviour {
+
+	public string sceneName;
+
+	// Use this for initialization
+	void Start () {
+		Button b = gameObject.GetComponent<Button> ();
+		b.onClick.AddListener(delegate() { LoadScene(sceneName); });
+	}
+
+	private void LoadScene(string level)
+	{
+		SceneManager.LoadScene (level);
+		//Application.LoadLevel (level);
+	}
+
+}
