aed2968f3a0debe30c307d7e2a2ca37e096ba54a
Chris Hedberg <christophhedberg@gmail.com>
2016-09-25 21:14:28 -0500

    Implemented First Person Camera

--- Assets/Scripts/PlayerController3rdPerson.cs
+++ Assets/Scripts/PlayerController3rdPerson.cs
@@ -2,7 +2,7 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController3rdPerson : MonoBehaviour {
     
 	public float speed;
 
